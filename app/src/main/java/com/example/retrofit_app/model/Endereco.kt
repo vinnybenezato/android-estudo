c39a849a4214a8ef6cba88b1f0ce6c310f7d817f
Vinicius Silveira Benezato <v.benezato@hotmail.com>
2024-05-28 11:54:50 -0300

    adição de "!" na mensagem de nao enconrado

--- app/src/main/java/com/example/retrofit_app/model/Endereco.kt
+++ app/src/main/java/com/example/retrofit_app/model/Endereco.kt
@@ -3,12 +3,7 @@ package com.example.retrofit_app.model
 class Endereco (
     val cep: String,
     val logradouro: String,
-    val complemento: String,
     val bairro: String,
     val localidade: String,
     val uf: String,
-    val ibge: String,
-    val gia: String,
-    val ddd: String,
-    val siafi: String
 )
